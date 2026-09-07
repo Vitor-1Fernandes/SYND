e8eb9c5782196c82cd41a98a694c7ea5e59439d4
Vitor Fernandes dos Santos <vitorfernandes11122006@gmail.com>
2026-09-07 20:47:58 -0300

    fix: allow frontend request

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,10 @@ app = FastAPI(title="API de Análise de Reuniões")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "https://synd-iot.vercel.app",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
